1c87341c24ce588da44938d1c08f89ad1aa5f581
Eleven <1656447904@qq.com>
2021-06-23 13:39:28 +0800

    main

--- sort-number/index.js
+++ sort-number/index.js
@@ -5,7 +5,7 @@ for (let i = 0; i < 50; ++i) {
     arr.push(random(1, 50))
 }
 /**
- * 产生随机整数，包含下限值，包括上限值
+ * 产生随机整数、包含下限-上限值
  * @param {Number} lower 下限
  * @param {Number} upper 上限
  * @return {Number} 返回在下限到上限之间的一个随机整数
@@ -15,6 +15,7 @@ function random(lower, upper) {
 }
 
 /**
+ * 数据交换函数
  * @param {Array} arr 数组
  * @param {Number} origin 需要交换的位置
  * @param {Number} target 等待交换的位置
@@ -28,7 +29,7 @@ function swap(arr, origin, target) {
 /**
  * 渲染html结构
  * @param {Object} origin 父元素
- * @param {array} target 数组
+ * @param {Array} target 数组
  */
 function createEle(origin, target) {
     origin.innerHTML = '';
@@ -43,9 +44,10 @@ function createEle(origin, target) {
 createEle(ul, arr);
 var li = ul.getElementsByTagName('li');
 
+
 /**
  * 选择排序
- * @param {array} origin 需要排序的数组
+ * @param {Array} origin 需要排序的数组
  */
 function selectSort(origin) {
     var len,
@@ -94,7 +96,7 @@ function selectSort(origin) {
 
 /**
  *  冒泡排序
- * @param {array} origin 
+ * @param {Array} origin 
  */
 function bubblingSort(origin) {
     //循环数组 找到最大的一位将最大的一位
@@ -123,7 +125,7 @@ function bubblingSort(origin) {
 
 /**
  * 插入排序
- * @param {array} target 需要排序的数组
+ * @param {Array} target 需要排序的数组
  */
 function insertSort(target) {
     // 记录数组长度
@@ -133,19 +135,74 @@ function insertSort(target) {
     var len = target.length;
     for (var i = 1; i < len; ++i) {
         if (target[i] < target[i - 1]) {
+            var temp = target[i];
             for (var j = i; j >= 0; --j) {
-                if (target[j - 1] > target[j]) {
-                    swap(target, j - 1, j)
-                    if (target[j - 1] > target[j - 2] && target[j - 1] < target[j]) {
-                        // 循环结束条件
-                        break;
-                    }
+                // if (target[j - 1] > target[j]) {
+                //     swap(target, j - 1, j)
+                //     if (target[j - 1] > target[j - 2] && target[j - 1] < target[j]) {
+                //         // 循环结束条件
+                //         break;
+                //     }
+                // }
+
+                if (j > 0 && target[j - 1] > temp) {
+                    target[j] = target[j - 1];
+                } else {
+                    target[j] = temp;
+                    break;
                 }
             }
         }
     }
 }
 
-insertSort(arr);
+// insertSort(arr);
 
+/**
+ * 快速排序
+ * @param {Array} target 
+ */
+function quickSort(target) {
+    /**
+     * 快速排序 辅助
+     * @param {Array} target 数组
+     * @param {Number} start 开始下标
+     * @param {Number} end 结束下标
+     */
+    function _quickSort(target, start, end) {
+        // 判断传入的数值是否有效
+        if (start >= end || start > end) return;
+        var key,
+            left,
+            right;
+        // 记录基准值
+        key = target[start];
+        left = start;
+        right = end;
+        // 循环函数判断条件为起始下标小于结束下标
+        for (; left < right;) {
+            // 并且向左循环时下标值大于基准值;
+            // 小于则停止循环
+            // 交换两个值
+            // Array[0 <-- length] 右向左
+            for (; left < right && target[right] >= key; --right);
+            swap(target, left, right);
+            // 从左向右循环
+            // Array[0 --> length] 左向右
+            for (; left < right && target[left] <= key; ++left);
+            swap(target, left, right);
+        }
+        // 到当前步骤时left与right相同，分区进行排序
+        return _quickSort(target, start, left - 1) || _quickSort(target, right + 1, end)
+    }
+    _quickSort(target, 0, target.length - 1)
+}
+
+
+// var myArr = [6, 7, 4, 3, 1, 2, 9, 8, 5];
+console.log(arr);
+quickSort(arr);
+
+
+console.log(arr);
 
